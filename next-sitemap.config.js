b38f70abd2cec7bf127386b2866518c22a2d6549
JP Branski <32647040+jpbranski@users.noreply.github.com>
2025-11-18 21:14:49 -0600

    Update site URL in next-sitemap configuration

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://www.careerquill.com',
+  siteUrl: process.env.SITE_URL || 'https://www.thecareerquill.com',
   generateRobotsTxt: true,
   generateIndexSitemap: false,
   exclude: ['/api/*'],
